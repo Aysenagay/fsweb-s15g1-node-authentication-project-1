0f97084647b49a9999704476a4658d02aadb9ad4
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-29 22:12:15 +0300

    proje tamamlandı.

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,3 +1,6 @@
+const userModel = require("../users/users-model");
+const bcryptjs = require("bcryptjs");
+
 /*
   Kullanıcının sunucuda kayıtlı bir oturumu yoksa
 
@@ -7,7 +10,15 @@
   }
 */
 function sinirli() {
-
+  try {
+    if (req.session && req.session.user_id > 0) {
+      next();
+    } else {
+      res.status(401).json({ message: "Geçemezsiniz!" });
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -19,7 +30,19 @@ function sinirli() {
   }
 */
 function usernameBostami() {
-
+  async function usernameBostami(req, res, next) {
+    try {
+      let { username } = req.body;
+      const isExist = await userModel.goreBul({ username: username });
+      if (isExist && isExist.length > 0) {
+        res.status(422).json({ message: "Username kullaniliyor" });
+      } else {
+        next();
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 /*
@@ -30,8 +53,32 @@ function usernameBostami() {
     "message": "Geçersiz kriter"
   }
 */
-function usernameVarmi() {
-
+async function usernameVarmi() {
+  try {
+    let { username } = req.body;
+    const isExist = await userModel.goreBul({ username: username });
+    if (isExist && isExist.length > 0) {
+      let user = isExist[0];
+      let isPasswordMatch = bcryptjs.compareSync(
+        req.body.password,
+        user.password
+      );
+      if (isPasswordMatch) {
+        req.dbUser = user;
+        next();
+      } else {
+        res.status(401).json({
+          message: "Geçersiz kriter",
+        });
+      }
+    } else {
+      res.status(401).json({
+        message: "Geçersiz kriter",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
 /*
@@ -43,7 +90,35 @@ function usernameVarmi() {
   }
 */
 function sifreGecerlimi() {
-
+  try {
+    let { password } = req.body;
+    if (!password || password.length < 3) {
+      res.status(422).json({ message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
 }
 
+function checkPayload(req, res, next) {
+  try {
+    let { username, password } = req.body;
+    if (!username || !password) {
+      res.status(422).json({ message: "Şifre 3 karakterden fazla olmalı" });
+    } else {
+      next();
+    }
+  } catch (error) {
+    next(error);
+  }
+}
 // Diğer modüllerde kullanılabilmesi için fonksiyonları "exports" nesnesine eklemeyi unutmayın.
+module.exports = {
+  usernameBostami,
+  checkPayload,
+  sifreGecerlimi,
+  usernameVarmi,
+  sinirli,
+};
